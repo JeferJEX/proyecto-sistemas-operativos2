fa678e5e267fe013fb861db92013946053bca644
Jeferson Josué Elel Xicay <jefersonxicay188@gmail.com>
2026-05-28 12:50:53 -0600

    Added restaurant frontend and API

--- api/index.js
+++ api/index.js
@@ -5,6 +5,32 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const menuItems = [
+    {
+        name: "Pizza Margherita",
+        price: 8.5,
+        description: "Tomate, mozzarella, albahaca"
+    },
+    {
+        name: "Ensalada César",
+        price: 7.0,
+        description: "Lechuga, pollo, parmesano, crutones"
+    },
+    {
+        name: "Burguer clásica",
+        price: 9.5,
+        description: "Carne de res, queso, lechuga, tomate"
+    }
+];
+
+app.get('/api/items', (req, res) => {
+    res.json(menuItems);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
